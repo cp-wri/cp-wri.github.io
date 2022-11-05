94c19731cdb9ea026e1c0ba0a27588243f725132
Ali Zulfikar <alizulfikar032@gmail.com>
2022-11-05 20:10:31 +0700

    PostCard: Fix flex title, tags, and readmore

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,65 +1,51 @@
 <template>
-  <div
-    class="
+  <div class="
       h-full
       overflow-hidden
       md:space-x-4
       text-gray-700
       rounded-lg
       shadow-xl
+      flex
+      flex-col
       dark:bg-gray-800 dark:text-gray-300
-    "
-  >
+    ">
     <g-link :to="`${post.node.path}/`" class="text-copy-primary">
-      <g-image
-        class="lg:h-48 md:h-36 w-full object-cover object-center"
-        :src="
-          post.node.thumbnail
-            ? post.node.thumbnail.src
-            : '/images/default-thumb.png'
-        "
-        alt="post.node.title"
-      />
+      <g-image class="lg:h-48 md:h-36 w-full object-cover object-center" :src="
+        post.node.thumbnail
+          ? post.node.thumbnail.src
+          : '/images/default-thumb.png'
+      " alt="post.node.title" />
     </g-link>
-    <div class="p-6">
-      <h2
-        class="
+    <div class="p-6 h-full flex flex-col justify-between">
+      <span>
+        <h2 class="
           tracking-widest
           text-xs
           title-font
           font-medium
           mb-1
-          text-secondary-500
-        "
-      >
-        {{ parentCategory(post.node.categories) }}
-      </h2>
-      <h1 class="title-font text-xl font-medium text-gray-900 mb-3">
-        <g-link :to="`${post.node.path}/`" class="text-copy-primary">
-          {{ post.node.title }}
-        </g-link>
-      </h1>
+          dark:text-secondary-300
+          text-secondary-600
+        ">
+          {{ parentCategory(post.node.categories) }}
+        </h2>
+        <h1 class="title-font text-xl font-medium text-gray-900 mb-3">
+          <g-link :to="`${post.node.path}/`" class="text-copy-primary">
+            {{ post.node.title }}
+          </g-link>
+        </h1>
+      </span>
       <p class="leading-relaxed mb-3">{{ post.node.summary }}</p>
       <div class="flex items-center flex-wrap">
-        <g-link
-          :to="`${post.node.path}/`"
-          class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0"
-          >Read More
-          <svg
-            class="w-4 h-4 ml-2"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
+        <g-link :to="`${post.node.path}/`" class="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0">Read More
+          <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
+            stroke-linecap="round" stroke-linejoin="round">
             <path d="M5 12h14"></path>
             <path d="M12 5l7 7-7 7"></path>
           </svg>
         </g-link>
-        <div
-          class="
+        <div class="
             inline-flex
             items-center
             lg:ml-auto
@@ -69,10 +55,9 @@
             text-sm
             mt-5
             md:mt-0
-          "
-        >
+          ">
           <span class="border-r-2 border-gray-200 px-3 mr-3">{{
-            post.node.date
+              post.node.date
           }}</span>
           <span class="mr-3">☕</span>
           <span>{{ post.node.timeToRead }} min read</span>
